3b0216aa5caf5a25256637859b13906c4793f55b
tlemesle <theo.lemesle42@gmail.com>
2021-02-04 17:18:09 +0100

    test

--- srcs/init.sh
+++ srcs/init.sh
@@ -21,7 +21,7 @@ ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 # SSL
 mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/nginx-selfsigned.crt -keyout /etc/nginx/ssl/nginx-selfsigned.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=tlemesle/CN=localhost"
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/nginx-selfsigned.crt -keyout /etc/nginx/ssl/nginx-selfsigned.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=root/CN=localhost"
 
 service nginx start
 
