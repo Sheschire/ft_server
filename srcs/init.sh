1014db68d50009e789cd5bffe3616e00bf6c84f4
tlemesle <theo.lemesle42@gmail.com>
2021-02-05 14:02:10 +0100

    test

--- srcs/init.sh
+++ srcs/init.sh
@@ -22,7 +22,7 @@ mkdir /etc/nginx/ssl
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=root/CN=localhost"
 
 # Authorization
-chown -R www-data /var/www/*
+chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
 
 service nginx start
